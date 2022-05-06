b7494c7045be62495488362b5f54f0a4c273e2e7
tahathedancer98 <bookingtaha98@gmail.com>
2022-05-06 16:53:38 +0200

    Insertion et récupération des données dans MONGO

--- 19-MONGO-START/app.js
+++ 19-MONGO-START/app.js
@@ -11,3 +11,37 @@ const userSchema = new mongoose.Schema({
     age: Number
 })
 
+const User = mongoose.model('User', userSchema)
+
+async function createdUser(doc){
+    const user = new User(doc)
+    try {
+        const result = user.save()
+        // const result2 = await User.remove({})// normalement ca va tout supprimé et c'est utils pour JEST
+    } catch (error) {
+        // ici on peut gérer les exc les cas où la promesse échoue
+    }
+    
+}
+
+const p1 = createdUser({
+    name: "Taha",
+    username : "ToTo",
+    isAdmin: true,
+    age: 23
+});
+const p2 = createdUser({
+    name: "Yassin",
+    username : "Yaya",
+    isAdmin: false,
+    age: 33
+});
+Promise.all([p1.p2]).then(async () => {
+    const all_docs = await User.find();
+    console.log(all_docs);
+    
+    const filtered_docs = await User.find({name:"Yassin"})
+    console.log(filtered_docs);
+    mongoose.connection.close()
+})
+
