7177c12f7f350879e787a75b699c7bc11a8a9f1e
tahathedancer98 <bookingtaha98@gmail.com>
2022-05-06 14:52:59 +0200

    Auth avec un token

--- 17-AUTHENTICATION-JWT/app.js
+++ 17-AUTHENTICATION-JWT/app.js
@@ -1,5 +1,6 @@
-// JWT Logic
-
+/*
+ *   JWT LOGIC
+*/
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 if (!process.env.JWT_SECRET_KEY) {
@@ -14,10 +15,19 @@ const JWT_SECRET_KEY = "12345678"
 const express = require('express');
 const app = express();
 
-// BDD 
+/*
+ *           B D D
+ */
 const Collection = require('../Collection');
 const Accounts = new Collection("Accounts");
 // const Cruds = new Collection("Cruds"); // une ressource CRUD générique, ex: Todos, items d'une liste de courses, etc... 
+
+
+
+/*
+ * PRE-POPULER LA BASE DE DONNES POUR VERIFIER QUE CA FONCTIONNE
+ * (juste pour des raisons pédagogiques, en vrai ne pas mettre les trucs en dessous.
+ */ 
 
 /* on va mettre des trucs en BDD pour bien séparer la logique d'Auth de la logique de Signin */
 
@@ -26,7 +36,7 @@ Accounts.memoryDb.set(2, {name: "Rémi", isAdmin: true});
 
 Accounts.memoryDb.set(0, {name: "PasRémi", isAdmin: false});
 // associated token : eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNjUxODM2Njg3fQ.AyilbQC2-Bu1f_X0SUIQ72_agVBQMxaeg0nQLB0xVrI , associated key : 12345678
-Accounts.id = 4;
+Accounts.id = 3; // 
 
 
 
@@ -55,6 +65,22 @@ app.get('/', (req, res) => {
 app.get('/moncompte', [authGuard], (req, res) => {
   const user = Accounts.getOne(req.user.id)
 
+  delete user.password; // on ne veut pas transmettre le Hash.
+
+  res.status(200).send({user});
+})
+
+
+// Route réservée aux admins
+app.get('/compte/:id', [authGuard], (req, res) => {
+  
+
+  // ROLE BASED ACCESS
+  const whoami = Accounts.getOne(req.user.id)
+  if (!whoami.isAdmin) return res.status(403).json({erreur: "Vous n'avez pas le droit d'accéder à ça"})
+
+  
+  const user = Accounts.getOne(parseInt(req.params.id))
   delete user.password; // on ne veut pas transmettre le Hash.
 
   res.status(200).send({user});
