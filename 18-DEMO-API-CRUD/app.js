9d143517ab1e7e7d7eb003c5be0ad00ff0567196
tahathedancer98 <bookingtaha98@gmail.com>
2022-05-06 15:11:05 +0200

    Ajouter la logique d'authentification

--- 18-DEMO-API-CRUD/app.js
+++ 18-DEMO-API-CRUD/app.js
@@ -8,8 +8,28 @@ const Accounts = new CollectionDB("Accounts");
 Courses.maBD.set(0, {idUser: 1, boissons : 10});
 Accounts.maBD.set(1, {name: "Rémi"});
 
+/* Logique d'authentification */
+function authGuard(req, res, next) {
+    const token = req.header('x-auth-token');
+    if (!token) return res.status(401).json({erreur: "Vous devez vous connecter"})
+  
+    try {
+      const decoded = jwt.verify(token, JWT_SECRET_KEY); 
+      req.user = decoded;
+      // Le middleware a fait son boulot et peut laisser la place au suivant.
+      next();
+    } catch (exc) {
+      return res.status(400).json({erreur: "Token Invalide"})
+    }
+  }
+
 app.get('/', (req , res) => {
-    //Tous les courses : Courses.all()
+    // Tous les courses : Courses.all()
+    // Tous les users : Accounts.all()
+    res.status(200).json({message: "Hello World"});
+})
+app.get('/', (req , res) => {
+    // Tous les courses : Courses.all()
     // Tous les users : Accounts.all()
     res.status(200).json({message: "Hello World"});
 })
